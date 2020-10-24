f24298b6bdebc10e429c68ddc5f112ccf5f66d83
jorcelinojunior <jorcelino@live.com>
2020-10-24 13:20:22 -0300

    Trabalhando na criação da tela do barbeiro. Projeto rodando normalmente.
    .

--- src/screens/Barber/index.js
+++ src/screens/Barber/index.js
@@ -1,11 +1,5 @@
-import {
-  useNavigation,
-  useRoute
-} from '@react-navigation/native';
-import React, {
-  useEffect,
-  useState
-} from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
 import Swiper from 'react-native-swiper';
 import Api from './../../Api';
 import BackIcon from './../../assets/back.svg';
@@ -27,7 +21,7 @@ import {
   UserFavButtom,
   UserInfo,
   UserInfoArea,
-  UserInfoName
+  UserInfoName,
 } from './styles';
 
 export default () => {
@@ -64,104 +58,53 @@ export default () => {
     navigation.goBack();
   };
 
-  return ( <
-    Container >
-    <
-    Scroller > {
-      userInfo.photos && userInfo.photos.length > 0 ? ( <
-        Swiper style = {
-          {
-            height: 240,
-            backgroundColor: '#ff00aa'
-          }
-        }
-        dot = {
-          < SwipeDot / >
-        }
-        activeDot = {
-          < SwipeActiveDot / >
-        }
-        paginationStyle = {
-          {
-            top: 15,
-            right: 15,
-            bottom: null,
-            left: null
-          }
-        }
-        autoplay = {
-          true
-        }
-        loop = {
-          true
-        } > {
-          userInfo.photos.map((item, key) => ( <
-            SwipeItem key = {
-              key
-            } >
-            <
-            SwipeImage source = {
-              {
-                uri: item.url,
-              }
-            }
-            resizeMode = "cover" /
-            >
-            <
-            /SwipeItem>
-          ))
-        } <
-        /Swiper>
-      ) : ( <
-        FakeSwiper / >
-      )
-    } <
-    PageBody >
-    <
-    UserInfoArea >
-    <
-    UserAvatar source = {
-      {
-        uri: userInfo.avatar,
-      }
-    }
-    /> <
-    UserInfo >
-    <
-    UserInfoName > {
-      userInfo.name
-    } < /UserInfoName> <
-    Stars stars = {
-      userInfo.stars
-    }
-    showNumber = {
-      true
-    }
-    /> <
-    /UserInfo> <
-    UserFavButtom >
-    <
-    FavoriteIcon width = "24"
-    height = "24"
-    fill = "#ff0000" / >
-    <
-    /UserFavButtom> <
-    /UserInfoArea> <
-    ServiceArea / >
-    <
-    TestimonialArea / >
-    <
-    /PageBody> <
-    /Scroller> <
-    BackButtom onPress = {
-      handleBackButtom
-    } >
-    <
-    BackIcon width = "44"
-    height = "44"
-    fill = "#fff" / >
-    <
-    /BackButtom> <
-    /Container>
+  return (
+    <Container>
+      <Scroller>
+        {userInfo.photos && userInfo.photos.length > 0 ? (
+          <Swiper
+            style={{height: 240, backgroundColor: '#ff00aa'}}
+            dot={<SwipeDot />}
+            activeDot={<SwipeActiveDot />}
+            paginationStyle={{top: 15, right: 15, bottom: null, left: null}}
+            autoplay={true}
+            loop={true}>
+            {userInfo.photos.map((item, key) => (
+              <SwipeItem key={key}>
+                <SwipeImage
+                  source={{
+                    uri: item.url,
+                  }}
+                  resizeMode="cover"
+                />
+              </SwipeItem>
+            ))}
+          </Swiper>
+        ) : (
+          <FakeSwiper />
+        )}
+        <PageBody>
+          <UserInfoArea>
+            <UserAvatar
+              source={{
+                uri: userInfo.avatar,
+              }}
+            />
+            <UserInfo>
+              <UserInfoName> {userInfo.name} </UserInfoName>
+              <Stars stars={userInfo.stars} showNumber={true} />
+            </UserInfo>
+            <UserFavButtom>
+              <FavoriteIcon width="24" height="24" fill="#ff0000" />
+            </UserFavButtom>
+          </UserInfoArea>
+          <ServiceArea />
+          <TestimonialArea />
+        </PageBody>
+      </Scroller>
+      <BackButtom onPress={handleBackButtom}>
+        <BackIcon width="44" height="44" fill="#fff" />
+      </BackButtom>
+    </Container>
   );
 };
